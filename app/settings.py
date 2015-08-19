3b8b8c3006988edae909696572f9554ee49a5f13
Your Name <you@example.com>
2015-08-19 14:01:26 -0600

    Next 2

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ki$8=s@=rbd%vf%gxz6307x06rn^3zf%jya$+9er@8sg*!=+rz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
